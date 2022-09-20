6932c37cf3f4191273c9a7cd8403dc04c0190dd6
QoriAlfatahP <113969392+QoriAlfatahP@users.noreply.github.com>
2022-09-20 14:08:41 +0700

    Add files via upload

---
+++ Praktikum2/Debug.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Debug : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        UnityEngine.Debug.Log("Hello World Qori");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
